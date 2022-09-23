1bc44eb2dd4a91226dbf3a3153373f5088fd5984
karthigayani <87833625+karthigayani@users.noreply.github.com>
2022-09-23 16:19:34 +0530

    Add files via upload
    JSON Resume

---
+++ Task3.1.js
@@ -0,0 +1,75 @@
+let Resume={
+    "KARTHIGAYANI.K":{
+       "E-mail" : "karthi1826@gmail.com",
+       "Mobile" : "+91 – 81000 88000."},
+   
+   "PROFILES":{
+       "github": "  ",
+       "LinkedIn": " "},
+   
+   "CAREER OBJECTIVE":"Electrical & Electronics Engineer with 2.5+ years of experience in Calibration, looking for an opportunity to utilize my acquired knowledge and skills for the growth and development of company.",
+   
+   "WORK EXPERIENCE":{
+        "Technical Lead and Senior Calibration Engineer, Sept 2017 to Aug 2019, Hi Tech Calibration Services":{
+            "ROLES AND RESPONSIBILITIES":[
+                "Developed new Scope by analyzing and interpreting the factors affecting the quality and precise nature of instruments and evaluating their uncertainty.",
+                "Promoted as the Authorized Person for certifying Instruments during the NABL Audit in Thermal, and Electro - Technical discipline.",
+                "Expanded the Scope of company by adding the Calibration of Infrared Thermometers and developed a new discipline, Optical (Light measurement).",
+                "Training of Higher end Instruments to Junior Calibration Engineers and providing training for newly joined technicians."]},
+       
+        "Calibration Engineer, Dec 2016 to Aug 2017, Hi Tech Calibration Services":{
+            "ROLES AND RESPONSIBILITIES":[
+                "Responsible for Laboratory's Calibration operations, and to secure that their services are performed according to ISO: IEC 17025 requirements and under NABL Accreditation",
+                "Responsible for all measurement methods, measurement standards, calibration instructions, measurement uncertainty calculations, calibration software and other tools that are used in the calibration Laboratory",
+                "Derived effective fault analysis techniques when instruments fail during Calibration.",
+                "Well-Versed with measurement method development, verification and uncertainty calculations.",
+                "Profound knowledge in Calibration of Oscilloscope, Power analyzer, Multifunction Calibrator, 6½ digit Multimeter, Decade resistance box, Signal conditioner, Transmitter, LCR meter, RTD, Thermocouple, Hygrometer, Pyrometer, Furnace, Illuminance & UV meter."]}},  
+    
+    "EDUCATION":[
+            "B.Tech,(EEE),PRIST University,Puducherry Campus", 
+            "Year Of Passing:2016, Percentage:85% ",
+            "HSC,Govt Girls Hr.Sec.School,Kadhirkamam ",
+            "Year Of Passing:2018, Percentage:75% ",
+            "SSLC,Kamban Govt.Hr.Sec.School,Nettapakkam ",
+            "Year Of Passing:2018, Percentage:80% "],
+    
+    "PROJECT UNDERGONE":[
+            "Transformer Core Saturation Elimination in T-STATCOM Using Cascaded Multilevel Convertor.",
+            "Description: Aimed to improve the overall power quality of the transmission system."],
+    
+    "CERTIFICATIONS" :[
+            "Participate in Implant training MARAPALAM SUBSTATION for one week in Government of Puducherry Electricity Department.",
+            "Basics of Electrical and Thermal Measurements and Uncertainty Concepts by NABL Lead Assessor.",
+            "Participated in value added training in “EMBEDDED SYSTEM DESIGN” for one day at premises ACCEL IT ACADEMY, Puducherry."],                       
+    
+    "TECHNICAL SKILLS":[
+            "Basics of C, C++, MS Office, MATLAB (Basics)",
+            "Having knowledge to work with Operating Systems like Windows 7 and Windows 10"],
+    
+    "EXTRA CURRICULAR ACTIVITIES":[
+        "Elected as PRESIDENT of Rotaract Club in 2015.",
+        "NSS Voluntary and attending blood camps.",
+        "Active Member in Juniper Organization of Social Services and have organized many events under the banner."],
+    
+    "AWARDS AND ACHIEVEMENTS":[
+        "Badminton under 14 at regional level [2nd place].",
+        "Essay competition at regional level [2nd place].",
+        "Quiz competition at school level [3nd place]."],
+    
+    "PERSONAL DETAILS":{
+        "Date of birth":"18.11.1994",
+        "Father’s Name":"Mr. S. KRISHNAMOORTHY",
+        "Gender ":"Female",
+        "Address":"No. 2/637, Iyyanar Koil Street, TC.Koot Road, Vanur TK, Villupuram DT - 605 111.",
+        "Nationality":"Indian",
+        "Languages known":"Tamil, English",
+        "Hobbies":"Gardening, Listening to music, Surfing Internet"},
+    
+    "DECLARATION":"I hereby declare that the particulars given herein are true to the best of my knowledge and belief",
+    
+    "Place:" : "Puducherry",
+    "Date:"  : "21-09-2022",
+    "Signature":"(KARTHIGAYANI .K)"
+
+} 
+console.log(Resume);
